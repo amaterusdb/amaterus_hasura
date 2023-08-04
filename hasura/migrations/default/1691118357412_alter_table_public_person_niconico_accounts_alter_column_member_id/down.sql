6e50308718e91de22efe66a00967424dff636ffd
aoirint <aoirint@gmail.com>
2023-08-04 12:06:02 +0900

    rename person_niconico_accounts.person_id

---
+++ hasura/migrations/default/1691118357412_alter_table_public_person_niconico_accounts_alter_column_member_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."person_niconico_accounts" rename column "person_id" to "member_id";
---
+++ hasura/migrations/default/1691118357412_alter_table_public_person_niconico_accounts_alter_column_member_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."person_niconico_accounts" rename column "member_id" to "person_id";
