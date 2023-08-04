9411a3cbf413ef9a0aa8af6c0b2b6a515d2e0ad8
aoirint <aoirint@gmail.com>
2023-08-04 12:05:00 +0900

    rename person_youtube_channels.person_id

---
+++ hasura/migrations/default/1691118260341_alter_table_public_person_youtube_channels_alter_column_member_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."person_youtube_channels" rename column "person_id" to "member_id";
---
+++ hasura/migrations/default/1691118260341_alter_table_public_person_youtube_channels_alter_column_member_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."person_youtube_channels" rename column "member_id" to "person_id";
