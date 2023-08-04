b533715c7b2a44537f13f64b6f8184e35a29cfa0
aoirint <aoirint@gmail.com>
2023-08-04 12:06:25 +0900

    rename person_niconico_communities

---
+++ hasura/migrations/default/1691118380040_rename_table_public_member_niconico_communities/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."person_niconico_communities" rename to "member_niconico_communities";
---
+++ hasura/migrations/default/1691118380040_rename_table_public_member_niconico_communities/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."member_niconico_communities" rename to "person_niconico_communities";
