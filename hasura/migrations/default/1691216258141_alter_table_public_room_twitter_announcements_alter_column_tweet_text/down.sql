939a2ab0dad9a3ad9afcc215accc8f4676c85dc7
aoirint <aoirint@gmail.com>
2023-08-05 15:17:46 +0900

    twitter_embed_html

---
+++ hasura/migrations/default/1691216258141_alter_table_public_room_twitter_announcements_alter_column_tweet_text/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."room_twitter_announcements" rename column "tweet_embed_html" to "tweet_text";
---
+++ hasura/migrations/default/1691216258141_alter_table_public_room_twitter_announcements_alter_column_tweet_text/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."room_twitter_announcements" rename column "tweet_text" to "tweet_embed_html";
