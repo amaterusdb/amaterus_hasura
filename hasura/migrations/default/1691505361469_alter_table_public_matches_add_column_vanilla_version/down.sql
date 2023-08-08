0da60a0c695c937e710f68634f787a88bf3c7080
aoirint <aoirint@gmail.com>
2023-08-08 23:36:16 +0900

    add matches.is_vanilla, vanilla_version

---
+++ hasura/migrations/default/1691505361469_alter_table_public_matches_add_column_vanilla_version/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."matches" add column "vanilla_version" text
+--  null;
---
+++ hasura/migrations/default/1691505361469_alter_table_public_matches_add_column_vanilla_version/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."matches" add column "vanilla_version" text
+ null;
