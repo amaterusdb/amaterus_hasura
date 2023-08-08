3f3f5db958d986b9710f0fe2165938cbf69a6d76
aoirint <aoirint@gmail.com>
2023-08-09 02:50:13 +0900

    add player_roles.document_url

---
+++ hasura/migrations/default/1691516997700_alter_table_public_player_roles_add_column_document_url/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."player_roles" add column "document_url" text
+--  null;
---
+++ hasura/migrations/default/1691516997700_alter_table_public_player_roles_add_column_document_url/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."player_roles" add column "document_url" text
+ null;
