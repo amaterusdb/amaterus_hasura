a72630d26ad1a3bc0f27459507910070cc54e245
aoirint <aoirint@gmail.com>
2023-08-08 23:58:53 +0900

    mod_versions.release_time

---
+++ hasura/migrations/default/1691506715250_alter_table_public_mod_versions_add_column_release_time/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."mod_versions" add column "release_time" timestamptz
+--  null;
---
+++ hasura/migrations/default/1691506715250_alter_table_public_mod_versions_add_column_release_time/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."mod_versions" add column "release_time" timestamptz
+ null;
