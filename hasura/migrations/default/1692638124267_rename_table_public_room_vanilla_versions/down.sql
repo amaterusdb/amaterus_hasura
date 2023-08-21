28fe5fddf598eebe4d8fbba40a72eb984729500a
aoirint <aoirint@gmail.com>
2023-08-22 02:15:44 +0900

    rename table room_vanilla_versions -> room_amongus_vanilla_versions

---
+++ hasura/migrations/default/1692638124267_rename_table_public_room_vanilla_versions/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."room_amongus_vanilla_versions" rename to "room_vanilla_versions";
---
+++ hasura/migrations/default/1692638124267_rename_table_public_room_vanilla_versions/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."room_vanilla_versions" rename to "room_amongus_vanilla_versions";
