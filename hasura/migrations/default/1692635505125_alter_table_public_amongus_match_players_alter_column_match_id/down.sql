ee1822a890e0805223fdb980c0b9d2d4356135ec
aoirint <aoirint@gmail.com>
2023-08-22 01:32:15 +0900

    rename column amongus_match_players.match_id -> amongus_match_id

---
+++ hasura/migrations/default/1692635505125_alter_table_public_amongus_match_players_alter_column_match_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."amongus_match_players" rename column "amongus_match_id" to "match_id";
---
+++ hasura/migrations/default/1692635505125_alter_table_public_amongus_match_players_alter_column_match_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."amongus_match_players" rename column "match_id" to "amongus_match_id";
