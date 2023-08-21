6a814cd0d1586f7a9321a5939bbf577fbd974946
aoirint <aoirint@gmail.com>
2023-08-22 02:13:58 +0900

    room_amongus_mods: unique keys

---
+++ hasura/migrations/default/1692638008542_alter_table_public_room_amongus_mods_add_unique_room_id_amongus_mod_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."room_amongus_mods" add constraint "room_amongus_mods_room_id_amongus_mod_id_key" unique ("room_id", "amongus_mod_id");
---
+++ hasura/migrations/default/1692638028060_alter_table_public_room_amongus_mods_add_unique_room_id_amongus_mod_version_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."room_amongus_mods" drop constraint "room_amongus_mods_room_id_amongus_mod_version_id_key";
