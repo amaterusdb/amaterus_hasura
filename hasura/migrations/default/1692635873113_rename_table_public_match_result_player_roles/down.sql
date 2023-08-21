96ff287d8260840c7a0f59c3fa9e9fff55a840dc
aoirint <aoirint@gmail.com>
2023-08-22 01:38:31 +0900

    rename table match_result_player_roles -> amongus_match_result_players

---
+++ hasura/migrations/default/1692635873113_rename_table_public_match_result_player_roles/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."amongus_match_result_players" rename to "match_result_player_roles";
---
+++ hasura/migrations/default/1692635873113_rename_table_public_match_result_player_roles/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."match_result_player_roles" rename to "amongus_match_result_players";
