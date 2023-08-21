45ce60a945a3cd6ae563089bf8657f4f7c05d887
aoirint <aoirint@gmail.com>
2023-08-22 02:16:29 +0900

    rename column room_amongus_vanilla_versions.vanilla_version_id -> amongus_vanilla_version_id

---
+++ hasura/migrations/default/1692638181274_alter_table_public_room_amongus_vanilla_versions_alter_column_vanilla_version_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."room_amongus_vanilla_versions" rename column "amongus_vanilla_version_id" to "vanilla_version_id";
---
+++ hasura/migrations/default/1692638181274_alter_table_public_room_amongus_vanilla_versions_alter_column_vanilla_version_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."room_amongus_vanilla_versions" rename column "vanilla_version_id" to "amongus_vanilla_version_id";
