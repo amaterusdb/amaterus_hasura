3ad299f9bd98e694265637f7bbd406bca95ff755
aoirint <aoirint@gmail.com>
2023-08-22 18:46:16 +0900

    twitter_tweet_images: tweet_id -> twitter_tweet_id

---
+++ hasura/migrations/default/1692697544428_alter_table_public_twitter_tweet_images_alter_column_tweet_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."twitter_tweet_images" rename column "twitter_tweet_id" to "tweet_id";
---
+++ hasura/migrations/default/1692697544428_alter_table_public_twitter_tweet_images_alter_column_tweet_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."twitter_tweet_images" rename column "tweet_id" to "twitter_tweet_id";
