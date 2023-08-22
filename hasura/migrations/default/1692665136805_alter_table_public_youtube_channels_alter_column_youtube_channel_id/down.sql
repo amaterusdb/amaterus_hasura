d88c84c5064f77689806a3a07c3d6d1069b213ce
aoirint <aoirint@gmail.com>
2023-08-22 09:45:48 +0900

    rename column youtube_channels.youtube_channel_id -> remote_youtube_channel_id

---
+++ hasura/migrations/default/1692665136805_alter_table_public_youtube_channels_alter_column_youtube_channel_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."youtube_channels" rename column "remote_youtube_channel_id" to "youtube_channel_id";
---
+++ hasura/migrations/default/1692665136805_alter_table_public_youtube_channels_alter_column_youtube_channel_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."youtube_channels" rename column "youtube_channel_id" to "remote_youtube_channel_id";
