2023c98557db59f5377f15f0008b8aed3d055172
aoirint <aoirint@gmail.com>
2023-08-28 12:57:44 +0900

    unique key

---
+++ hasura/migrations/default/1693195054829_alter_table_public_mariokart8deluxe_courses_add_unique_console_name_name/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."mariokart8deluxe_courses" drop constraint "mariokart8deluxe_courses_console_name_name_key";
---
+++ hasura/migrations/default/1693195054829_alter_table_public_mariokart8deluxe_courses_add_unique_console_name_name/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."mariokart8deluxe_courses" add constraint "mariokart8deluxe_courses_console_name_name_key" unique ("console_name", "name");
