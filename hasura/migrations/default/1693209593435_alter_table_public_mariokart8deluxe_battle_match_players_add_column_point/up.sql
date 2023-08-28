897f10e8de076c418a7cdbecb4ecfb834f22b0ac
aoirint <aoirint@gmail.com>
2023-08-28 17:00:31 +0900

    add point column

---
+++ hasura/migrations/default/1693209593435_alter_table_public_mariokart8deluxe_battle_match_players_add_column_point/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."mariokart8deluxe_battle_match_players" add column "point" integer
+ null;
---
+++ hasura/migrations/default/1693209609409_alter_table_public_mariokart8deluxe_race_players_add_column_point/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."mariokart8deluxe_race_players" add column "point" integer
+ null;
