e4b1517e4b3543850917a504455bcb6e91eb8d93
aoirint <aoirint@gmail.com>
2023-08-28 15:36:17 +0900

    add column mariokart8deluxe_battle_courses.index

---
+++ hasura/migrations/default/1693204558528_alter_table_public_mariokart8deluxe_battle_courses_add_column_index/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."mariokart8deluxe_battle_courses" add column "index" integer
+ not null;
---
+++ hasura/migrations/default/1693204564509_alter_table_public_mariokart8deluxe_battle_courses_add_unique_index/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."mariokart8deluxe_battle_courses" add constraint "mariokart8deluxe_battle_courses_index_key" unique ("index");
