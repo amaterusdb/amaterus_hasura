94679eb93f9c348892071490fe8e25a73d16cb98
aoirint <aoirint@gmail.com>
2023-08-28 13:16:15 +0900

    unique key

---
+++ hasura/migrations/default/1693196171556_alter_table_public_mariokart8deluxe_courses_add_unique_mariokart8deluxe_cup_id_in_cup_index/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."mariokart8deluxe_courses" drop constraint "mariokart8deluxe_courses_mariokart8deluxe_cup_id_in_cup_index_key";
---
+++ hasura/migrations/default/1693196171556_alter_table_public_mariokart8deluxe_courses_add_unique_mariokart8deluxe_cup_id_in_cup_index/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."mariokart8deluxe_courses" add constraint "mariokart8deluxe_courses_mariokart8deluxe_cup_id_in_cup_index_key" unique ("mariokart8deluxe_cup_id", "in_cup_index");
