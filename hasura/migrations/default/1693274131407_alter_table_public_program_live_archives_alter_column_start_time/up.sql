4bf8b7c71a0ec0ba9968b8b8e12de9ff709347de
aoirint <aoirint@gmail.com>
2023-08-29 10:55:49 +0900

    program_live_archives.start_time, end_time: nullable

---
+++ hasura/migrations/default/1693274131407_alter_table_public_program_live_archives_alter_column_start_time/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."program_live_archives" alter column "start_time" drop not null;
---
+++ hasura/migrations/default/1693274134982_alter_table_public_program_live_archives_alter_column_end_time/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."program_live_archives" alter column "end_time" drop not null;
