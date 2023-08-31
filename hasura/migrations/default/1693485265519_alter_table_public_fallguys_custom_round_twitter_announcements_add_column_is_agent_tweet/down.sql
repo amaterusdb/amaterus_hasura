4c7d37f905a067c63c888dfe78256fe187ef8392
aoirint <aoirint@gmail.com>
2023-08-31 21:34:44 +0900

    add column fallguys_custom_round_twitter_announcements.is_agent_tweet

---
+++ hasura/migrations/default/1693485265519_alter_table_public_fallguys_custom_round_twitter_announcements_add_column_is_agent_tweet/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."fallguys_custom_round_twitter_announcements" add column "is_agent_tweet" boolean
+--  null;
---
+++ hasura/migrations/default/1693485265519_alter_table_public_fallguys_custom_round_twitter_announcements_add_column_is_agent_tweet/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."fallguys_custom_round_twitter_announcements" add column "is_agent_tweet" boolean
+ null;
