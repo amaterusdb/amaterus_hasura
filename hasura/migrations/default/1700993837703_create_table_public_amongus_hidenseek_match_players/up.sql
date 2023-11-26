01a5ff4797809cb0feb382eb819c5ceb9247746a
aoirint <aoirint@gmail.com>
2023-11-26 19:17:24 +0900

    create table amongus_hidenseek_match_players

---
+++ hasura/migrations/default/1700993837703_create_table_public_amongus_hidenseek_match_players/up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "public"."amongus_hidenseek_match_players" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "amongus_hidenseek_match_id" uuid NOT NULL, "nickname" text NOT NULL, "person_id" uuid, PRIMARY KEY ("id") , FOREIGN KEY ("amongus_hidenseek_match_id") REFERENCES "public"."amongus_hidenseek_matches"("id") ON UPDATE cascade ON DELETE cascade, FOREIGN KEY ("person_id") REFERENCES "public"."persons"("id") ON UPDATE set null ON DELETE set null, UNIQUE ("amongus_hidenseek_match_id", "nickname"), UNIQUE ("amongus_hidenseek_match_id", "person_id"));
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_amongus_hidenseek_match_players_updated_at"
+BEFORE UPDATE ON "public"."amongus_hidenseek_match_players"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_amongus_hidenseek_match_players_updated_at" ON "public"."amongus_hidenseek_match_players"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
