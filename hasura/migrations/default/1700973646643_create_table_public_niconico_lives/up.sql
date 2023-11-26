4551f744b09a0eae0d01c76cdafbe67865222e94
aoirint <aoirint@gmail.com>
2023-11-26 13:41:00 +0900

    create table niconico_lives

---
+++ hasura/migrations/default/1700973646643_create_table_public_niconico_lives/up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "public"."niconico_lives" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "remote_niconico_content_id" text NOT NULL, "title" text NOT NULL, "niconico_account_id" uuid, "niconico_community_id" uuid, "niconico_channel_id" uuid, "start_time" timestamptz, "end_time" timestamptz, PRIMARY KEY ("id") , FOREIGN KEY ("niconico_account_id") REFERENCES "public"."niconico_accounts"("id") ON UPDATE set null ON DELETE set null, FOREIGN KEY ("niconico_channel_id") REFERENCES "public"."niconico_channels"("id") ON UPDATE set null ON DELETE set null, FOREIGN KEY ("niconico_community_id") REFERENCES "public"."niconico_communities"("id") ON UPDATE set null ON DELETE set null, UNIQUE ("remote_niconico_content_id"));
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_niconico_lives_updated_at"
+BEFORE UPDATE ON "public"."niconico_lives"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_niconico_lives_updated_at" ON "public"."niconico_lives"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
