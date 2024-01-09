8142ecf57129ec5d7a0abfc010c0a7cc9e269351
aoirint <aoirint@gmail.com>
2024-01-09 19:08:15 +0900

    create table lethalcompany_ships

---
+++ hasura/migrations/default/1704794873599_create_table_public_lethalcompany_ships/up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "public"."lethalcompany_ships" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "program_id" uuid NOT NULL, "start_time" uuid, "end_time" uuid, "index" integer, PRIMARY KEY ("id") , FOREIGN KEY ("program_id") REFERENCES "public"."programs"("id") ON UPDATE cascade ON DELETE cascade, UNIQUE ("program_id", "index"));
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_lethalcompany_ships_updated_at"
+BEFORE UPDATE ON "public"."lethalcompany_ships"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_lethalcompany_ships_updated_at" ON "public"."lethalcompany_ships"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
