2afd9fb50ceef58647b685ac39cd552669fc39b3
aoirint <aoirint@gmail.com>
2024-01-09 19:43:46 +0900

    add column start_time, end_time

---
+++ hasura/migrations/default/1704797012368_alter_table_public_lethalcompany_ship_days_add_column_start_time/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."lethalcompany_ship_days" add column "start_time" timestamptz
+ null;
---
+++ hasura/migrations/default/1704797019186_alter_table_public_lethalcompany_ship_days_add_column_end_time/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."lethalcompany_ship_days" add column "end_time" timestamptz
+ null;
