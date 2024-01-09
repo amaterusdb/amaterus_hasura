817ee1521aa8b905c54f752388a5b31a0bf5485f
aoirint <aoirint@gmail.com>
2024-01-09 19:37:51 +0900

    add column absolute_day_number

---
+++ hasura/migrations/default/1704796667728_alter_table_public_lethalcompany_ship_days_add_column_absolute_day_number/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."lethalcompany_ship_days" add column "absolute_day_number" integer
+--  null;
---
+++ hasura/migrations/default/1704796667728_alter_table_public_lethalcompany_ship_days_add_column_absolute_day_number/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."lethalcompany_ship_days" add column "absolute_day_number" integer
+ null;
