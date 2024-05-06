109b860a9865a0c8f0f2372f4fb6aa749be3373b
aoirint <aoirint@gmail.com>
2024-05-06 15:06:16 +0900

    add youtube_channels.auto_update_enabled

---
+++ hasura/migrations/default/1714975572201_alter_table_public_youtube_channels_add_column_auto_update_enabled/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."youtube_channels" add column "auto_update_enabled" boolean
+--  not null default 'true';
---
+++ hasura/migrations/default/1714975572201_alter_table_public_youtube_channels_add_column_auto_update_enabled/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."youtube_channels" add column "auto_update_enabled" boolean
+ not null default 'true';
