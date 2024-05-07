9a7653be6863cc5b40b8a82ebc10255765e51cfd
aoirint <aoirint@gmail.com>
2024-05-08 01:26:26 +0900

    add crawler__youtube_channel_video_configs.found_at

---
+++ hasura/migrations/default/1715099156905_alter_table_public_crawler__youtube_channel_video_configs_add_column_found_at/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."crawler__youtube_channel_video_configs" add column "found_at" timestamptz
+--  not null;
